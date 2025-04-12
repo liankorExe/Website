5127284cb8fda811a1a4462f08700b1c98f3f721
PYRAMID <contact@epworld.me>
2025-04-12 13:45:28 +0200

    Remove Nuxt.js

--- src/pages/index.vue
+++
@@ -1,7 +0,0 @@
-<template>
-    <div>
-        <h1 class="text-purpleprimary-100 text-3xl font-poppins font-bold underline">
-            Oh ! C'est le site de OpenMC ou je rêve ?
-        </h1>
-    </div>
-</template>
