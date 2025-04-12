8e60245c2bbf79403ffddab88f3798b6be90bfe4
PYRAMID <contact@epworld.me>
2025-04-12 15:54:54 +0200

    Navbar

---
+++ config.ts
@@ -0,0 +1,6 @@
+export const NAVBAR_LINKS = [
+    { href: "/", label: "Home" },
+    { href: "/statistiques", label: "Statistiques" },
+    { href: "https://wiki.openmc.fr/", label: "Wiki", external: true },
+    { href: "https://discord.gg/H7DrUjHw7q", label: "Discord", external: true },
+];
