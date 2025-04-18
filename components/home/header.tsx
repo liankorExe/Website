1ed4874047f4e2943f4e818cd13589151a6ed427
Alexis <awaknocode@gmail.com>
2025-04-18 17:14:18 +0200

    feat: add new page /rejoindre with server IP copied system

--- components/home/header.tsx
+++ components/home/header.tsx
@@ -5,6 +5,7 @@ import { ArrowRightIcon } from "lucide-react";
 import Link from "next/link";
 import { DotPattern } from "@/components/magicui/dot-pattern";
 import { Button } from "@/components/ui/button";
+import homePageImage from "@/public/placeholder/homepage.webp";
 
 export default function Header() {
     return (
@@ -71,7 +72,7 @@ export default function Header() {
 
                 <div className="mt-10 w-full max-w-5xl relative rounded-4xl overflow-hidden">
                     <Image
-                        src={`./placeholder/homepage.webp`}
+                        src={homePageImage}
                         alt="Placeholder"
                         width={1280}
                         height={720}
--- components/navigation/navbar.tsx
+++ components/navigation/navbar.tsx
@@ -6,11 +6,14 @@ import { Button } from "../ui/button";
 import { ArrowRightIcon } from "lucide-react"
 import { NAVBAR_LINKS } from "@/config";
 import { useEffect, useState } from "react";
+import Logo from "@/public/branding/logo.webp";
+import { useRouter } from "next/navigation";
 
 export default function Navbar() {
 
     const [isScrolled, setIsScrolled] = useState(false);
     const [menuOpen, setMenuOpen] = useState(false);
+    const router = useRouter();
 
     useEffect(() => {
         const handleScroll = () => setIsScrolled(window.scrollY > 20);
@@ -45,7 +48,7 @@ export default function Navbar() {
             >
                 <div className="flex items-center gap-4">
                     <Link href="/">
-                        <Image src={`./branding/logo.webp`} alt="Logo" width={200} height={50} />
+                        <Image src={Logo} alt="Logo" width={200} height={50} />
                     </Link>
                 </div>
                 <div className="hidden md:flex flex-grow justify-center gap-4 items-center">
@@ -65,7 +68,7 @@ export default function Navbar() {
                     ))}
                 </div>
                 <div className="hidden md:flex">
-                    <Button className="group">
+                    <Button className="group" onClick={() => router.push("/rejoindre")}>
                         Rejoindre le Serveur
                         <ArrowRightIcon
                             className="-me-1 opacity-60 transition-transform group-hover:translate-x-0.5"
